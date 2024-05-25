13264d4b9f806ac20393daebe6478059c8341579
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2024-05-25 17:13:57 -0300

    file: without component CnpjInfo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@ import { isMobile } from 'react-device-detect';
 
 //import { FormatCnpj } from './components/Formats';
 //import { validateCNPJ } from './components/ValidateRules';
-import CnpjInfo from './components/CnpjInfo';
+//import CnpjInfo from './components/CnpjInfo';
+
+const CnpjInfo = ({ cnpjData }) => {
+  if (!cnpjData) {
+    return null
+  }
+  
+  return (<div className='cnpj-info text-white'>
+    <h2>Informações do CNPJ: {cnpjData.cnpj}</h2>
+    <p><strong>Status:</strong> {cnpjData.status}</p>
+    <p><strong>Nome:</strong> {cnpjData.nome}</p>
+    <p><strong>Fantasia:</strong> {cnpjData.fantasia}</p>
+    <p><strong>Abertura:</strong> {cnpjData.abertura}</p>
+    <p><strong>Telefone:</strong> {cnpjData.telefone}</p>
+    {cnpjData.email && <p><strong>Email:</strong> {cnpjData.email}</p>}
+    <p><strong>Endereço:</strong> {cnpjData.logradouro}, {cnpjData.numero} <br /> {cnpjData.municipio}, {cnpjData.bairro} ({cnpjData.uf})</p>
+  </div>);
+}
 
 const validateCNPJ = (cnpj) => {
     cnpj = cnpj.replace(/[^\d]+/g, '');
